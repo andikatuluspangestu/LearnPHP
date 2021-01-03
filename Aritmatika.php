70e9fc43ea9b5bc9e7691dc6b0ba2383b8b1ac5b
andikatuluspangestu <andikatulusp@gmail.com>
2021-01-03 21:10:14 +0700

    Update

---
+++ Aritmatika.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aritmatika PHP</title>
+</head>
+<body>
+
+	<!-- Operator Aritmatika PHP -->
+	<?php 
+
+		echo 2 + 3;	 // Penjumlahan
+		echo 4 - 2;	 // Pengurangan
+		echo 2 * 3;	 // Perkalian
+		echo 2 / 3;	 // Pembagian
+		echo 10 % 2; // Modulus atau Hasil Bagi
+
+	?>
+
+	<!-- JANGAN LUPA TITIK KOMA -->
+
+</body>
+</html>
---
+++ LatihanPertama.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan Pertama</title>
+</head>
+<body>
+	<?php 
+
+		// Menampilkan Teks "Dolby" ke layar
+		echo 'Dolby';
+
+		// Membuat Perkalian, Pembagian dan Modulus 
+		
+		echo 90 * 2;	 // Perkalian
+		echo 18 / 4;	 // Pembagian
+		echo 24 % 2;     // Modulus atau Hasil Bagi
+
+	?>
+
+
+	<!-- JANGAN LUPA TITIK KOMA -->
+
+</body>
+</html>
